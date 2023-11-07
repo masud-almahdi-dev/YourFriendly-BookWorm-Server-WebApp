bbed88f22248f549946a961f21732fc02ead5fe8
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-11-08 04:57:41 +0600

    created get books

--- index.js
+++ index.js
@@ -8,31 +8,37 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    app.get('/', async(req, res) => {
-      await client.db("admin").command({ ping: 1 });
-      res.send("Pinged your deployment. You successfully connected to MongoDB!");
-    })
-    const users = client.db('BookWormDB').collection('users');
-    const books = client.db('BookWormDB').collection('books');
-    const categories = client.db('BookWormDB').collection('categories');
+    try {
+        app.get('/', async (req, res) => {
+            await client.db("admin").command({ ping: 1 });
+            res.send("Pinged your deployment. You successfully connected to MongoDB!");
+        })
+        const users = client.db('BookWormDB').collection('users');
+        const books = client.db('BookWormDB').collection('books');
+        const categories = client.db('BookWormDB').collection('categories');
 
-    app.get('/categories', async(req, res) => {
-      const cursor = categories.find();
-      const result = await cursor.toArray();
-      res.send(result)
-    })
-    
-  } finally {
-  }
+        app.get('/categories', async (req, res) => {
+            const cursor = categories.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/books', async (req, res) => {
+            const cursor = books.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+    } finally {
+    }
 }
 run().catch(console.dir);
 
