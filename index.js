24c9c4cc99152650473cf1b9658984e6a37a55c7
LastWorldDeer <functionnotworkingcorrectly@gmail.com>
2023-11-08 04:53:32 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("hello");
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json());
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    //await client.connect();
+    app.get('/', async(req, res) => {
+      await client.db("admin").command({ ping: 1 });
+      res.send("Pinged your deployment. You successfully connected to MongoDB!");
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`App listening on port: ${port}`)
+})
